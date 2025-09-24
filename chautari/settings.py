11008520aa581b16f302f39bca7cea636c376fa6
Sulav Maharjan <sulavmaharjan63@gmail.com>
2025-09-24 13:31:00 +0545

    implement logging using rich library

--- chautari/settings.py
+++ chautari/settings.py
@@ -167,3 +167,36 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": crontab(minute="*/15"),
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+    },
+    "formatters": {
+        "rich": {"datefmt": "[%X]"},
+    },
+    "handlers": {
+        "console": {
+            "class": "rich.logging.RichHandler",
+            "filters": ["require_debug_true"],
+            "formatter": "rich",
+            "level": "DEBUG",
+            "rich_tracebacks": True,
+            "tracebacks_show_locals": True,
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": [],
+            "level": "INFO",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
